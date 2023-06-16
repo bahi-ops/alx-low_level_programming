9fd9fefbb2af1596858c1ea6a2c8d65a1e66dd2b
DjangoSpop <behodjango@gmail.com>
2023-06-16 14:38:02 +0300

    yse

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main(void)
+/**
+ * Entry point of the program.
+ * Prints the message "Programming is like building a multilingual puzzle."
+ * Returns 0 to indicate successful execution.
+ */
+int main(void) /*main funtion*/
 {
 puts("Programming is like building a multilingual puzzle");
-return (0);
+ return (0);/*returns*/
 }
