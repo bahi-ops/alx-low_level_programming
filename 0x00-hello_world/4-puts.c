072829864417afd9d666f183d290292a0ba56224
DjangoSpop <behodjango@gmail.com>
2023-06-16 19:58:28 +0300

    yes we can

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-/**
- * @brief Entry point of the program.
- *
- * Prints the message "Programming is like building a multilingual puzzle."
- *
- * @return The return value 0 to indicate successful execution.
+ /**
+ * main - main block
+ * Return: 0
  */
 int main(void) 
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
  return (0);
 }
