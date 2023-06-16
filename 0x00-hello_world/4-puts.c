5f4af85819edbeda5f3e86f3446352cca7d61b19
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-06-17 00:36:07 +0300

    when you have a dream you must go for that dream

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
-* main main.
-* return0
-* description: main takeis a in and then prints it with puts.
-*/
+/**
+ *main - main block
+ *return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
