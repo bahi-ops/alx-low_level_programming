7ed174cd5576a4b5add2a523dd5ffc2506fe6ffb
DjangoSpop <behodjango@gmail.com>
2023-06-20 00:09:31 +0300

    a new chair begins a new journey

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
-	char letter = 'z';
-
-	while (letter >= 'a')
-	{
-		putchar(letter);
-		letter--;
-	}
-
-	putchar('\n');
-
-	return 0;
+char letter = 'z';
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+putchar('\n');
+return (0);
 }
