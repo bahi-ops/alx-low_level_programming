aee8b85be662cd90365805d9623840b0c06fca3b
DjangoSpop <behodjango@gmail.com>
2023-06-20 00:30:39 +0300

    a new chair begins a new journey

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
-    int num1;
-    int num2;
-
-    for (num1 = 0; num1 <= 99; num1++)
-    {
-        for (num2 = num1 + 1; num2 <= 99; num2++)
-        {
-            putchar((num1 / 10) + 48);
-            putchar((num1 % 10) + 48);
-            putchar(' ');
-            putchar((num2 / 10) + 48);
-            putchar((num2 % 10) + 48);
-
-            if (num1 != 98 || num2 != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return 0;
+int num1;
+int num2;
+for (num1 = 0; num1 <= 99; num1++)
+{
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+putchar((num1 / 10) + 48);
+putchar((num1 % 10) + 48);
+putchar(' ');
+putchar((num2 / 10) + 48);
+putchar((num2 % 10) + 48);
+if (num1 != 98 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
