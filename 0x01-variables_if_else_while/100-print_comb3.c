91a744eb42118de0e480bde28791e233e96ec3f1
DjangoSpop <behodjango@gmail.com>
2023-06-20 00:18:18 +0300

    a new chair begins a new journey

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
-    int tensDigit;
-    int onesDigit;
-
-    for (tensDigit = 0; tensDigit <= 8; tensDigit++)
-    {
-        for (onesDigit = tensDigit + 1; onesDigit <= 9; onesDigit++)
-        {
-            putchar(tensDigit + 48);
-            putchar(onesDigit + 48);
-
-            if (tensDigit != 8 || onesDigit != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return 0;
+int tensDigit;
+int onesDigit;
+for (tensDigit = 0; tensDigit <= 8; tensDigit++)
+{
+for (onesDigit = tensDigit + 1; onesDigit <= 9; onesDigit++)
+{
+putchar(tensDigit + 48);
+putchar(onesDigit + 48);
+if (tensDigit != 8 || onesDigit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
