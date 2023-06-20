998361f8d4290a06cc0179e0fe5349e5f82e4fae
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-06-20 17:11:31 +0300

    c is the best make u understand how to code

---
+++ 0x02-funtions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>;
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void main(void){
+char letter;
+ for(letter = 'a';letter <= 'z'; letter++ ){
+letter ++;
+_putchar(letter);
+}
+_putchar(/nletter);
+}
