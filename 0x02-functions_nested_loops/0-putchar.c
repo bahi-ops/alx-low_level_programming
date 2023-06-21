970bbcc732bf5ac203bc5da33323bc27f59c3020
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-06-22 01:39:15 +0300

    hi lets do this

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main - check the code
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void) {
-char str[] = "_putchar";
-for (int i = 0; str[i] != '\0'; i++) {
-_putchar(str[i]);
-}
-_putchar('\n');
-return 0;
+    char* message = "_putchar\n";
+    int i = 0;
+
+    while (message[i] != '\0') {
+        putchar(message[i]);
+        i++;
+    }
+
+    return 0;
 }
