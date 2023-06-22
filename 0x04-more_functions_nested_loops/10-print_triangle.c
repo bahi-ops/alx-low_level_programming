8746c0cef64d420c3ea36c94089f5ac23a5cc72b
DjangoSpop <behodjango@gmail.com>
2023-06-22 23:15:55 +0300

    yesa

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,23 @@
-i#include "main.h"
+#include "main.h"
 
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
+ * print_triangle - Prints a triangle using the '#' character.
+ * @size: The size of the triangle.
  */
 void print_triangle(int size)
 {
-	if (size <= 0)
-	{
+	int i, j;
+
+	if (size <= 0) {
 		_putchar('\n');
-	} else
-	{
-		int i, j;
+		return;
+	}
 
-		for (i = 1; i <= size; i++)
-		{
-			for (j = i; j < size; j++)
-			{
-				_putchar(' ');
-			}
-
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
-			}
-
-			_putchar('\n');
+	for (i = 0; i < size; i++) {
+		for (j = 0; j <= i; j++) {
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
 }
+
