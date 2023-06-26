0b2b86f2c9d977d613c956b27d62de5b74a20584
DjangoSpop <behodjango@gmail.com>
2023-06-26 22:04:54 +0300

    its only the begining of my software engineering caree

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
 * main - check the code
 *
 * Return: Always 0.
 */
-void reset_to_98(int *n)/*this funtion illustrates how to make a pointer in c*/
+void reset_to_98(int *n)
 {
-*n = 908;
+*n = 98;
 }
