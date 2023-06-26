8c16d14b0c4ef6faa8d39764b52f9d39917ed54f
DjangoSpop <behodjango@gmail.com>
2023-06-26 22:19:38 +0300

    we can

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * main - check the code
+ * _strlen - calculate the length of a string.
  *
- * Return: Always 0.
+ * @s: the string to calculate it's length.
+ * Return: length of a string.
  */
 int _strlen(char *s)
 {
-  int i;
-  int length= 0;
-  for (i= 0; s[i] != '\0'; i++) ;
-  {
-    length++;
-  }
-  return (length);
+int i;
+int length = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+length++;
+}
+return (length);
 }
