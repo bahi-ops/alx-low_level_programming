e3dc93a05f6136a2e796aaa9a77f92bfa6492356
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-07-18 00:58:23 +0300

    we can code until we die

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
-
 /**
  * new_dog - Creates a new dog.
  * @name: Name of the dog
@@ -13,34 +12,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    /* Calculate the lengths of the name and owner strings */
-    size_t name_len = strlen(name);
-    size_t owner_len = strlen(owner);
-
-    /* Allocate memory for the new dog struct */
-    dog_t *new_dog_ptr = malloc(sizeof(dog_t));
-    if (new_dog_ptr == NULL)
-        return NULL;
-
-    /* Allocate memory for the name and owner strings */
-    new_dog_ptr->name = malloc(name_len + 1); /* +1 for the null-terminator */
-    new_dog_ptr->owner = malloc(owner_len + 1);
-
-    /* Check if memory allocation was successful */
-    if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL) {
-        free(new_dog_ptr->name);
-        free(new_dog_ptr->owner);
-        free(new_dog_ptr);
-        return NULL;
-    }
-
-    /* Copy the name and owner strings to the new memory locations */
-    strcpy(new_dog_ptr->name, name);
-    strcpy(new_dog_ptr->owner, owner);
-
-    /* Assign the age */
-    new_dog_ptr->age = age;
-
-    /* Return the pointer to the new dog */
-    return new_dog_ptr;
+/* Calculate the lengths of the name and owner strings */
+size_t name_len = strlen(name);
+size_t owner_len = strlen(owner);
+/* Allocate memory for the new dog struct */
+dog_t *new_dog_ptr = malloc(sizeof(dog_t));
+if (new_dog_ptr == NULL)
+return NULL;
+/* Allocate memory for the name and owner strings */
+new_dog_ptr->name = malloc(name_len + 1); /* +1 for the null-terminator */
+new_dog_ptr->owner = malloc(owner_len + 1);
+/* Check if memory allocation was successful */
+if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL) {
+free(new_dog_ptr->name);
+free(new_dog_ptr->owner);
+free(new_dog_ptr);
+return NULL;
+}
+/* Copy the name and owner strings to the new memory locations */
+strcpy(new_dog_ptr->name, name);
+strcpy(new_dog_ptr->owner, owner);
+/* Assign the age */
+new_dog_ptr->age = age;
+/* Return the pointer to the new dog */
+return new_dog_ptr;
 }
