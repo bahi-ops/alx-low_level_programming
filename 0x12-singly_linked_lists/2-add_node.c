907e14ffac5761faa1981e70c31fc8c31623cb08
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-07-28 01:56:32 +0300

    we can do this

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-
 /**
- * add_node - adds a new node at the beginning of a list
- *@head: pointer to the head of the list
- *@str: string to be added
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to list
+ * @str: string to add in the node
  *
- * Return: returns the address to the new element or NULL
- * if failed
-*/
-
+ * Return: the address of the new element
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (dup == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
-
+list_t *new;
+unsigned int len = 0;
+while (str[len])
+len++;
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
+new->str = strdup(str);
+new->len = len;
+new->next = (*head);
+(*head) = new;
+return (*head);
 }
-
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,17 @@
+#include <stdlib.h>
 #include "lists.h"
-
 /**
- *free_list - Frees a list
- *@head: A pointer to the list head
- *
+ * free_list - frees a linked list
+ * @head: pointer to list
  */
-
 void free_list(list_t *head)
 {
-	list_t *tmp;
-
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
-	}
+list_t *tmp;
+while (head)
+{
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
+}
 }
